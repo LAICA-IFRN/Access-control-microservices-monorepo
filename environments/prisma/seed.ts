487348b48b66035cfc632fdc951067b986c251d6
HilquiasAbias <hilquias.dev@gmail.com>
2023-10-31 19:35:21 -0300

    preparando para teste no servidor

--- environments/prisma/seed.ts
+++ environments/prisma/seed.ts
@@ -1,32 +1,32 @@
-// import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
-// async function main() {
-//   await prisma.environment.create({
-//     data: {
-//       name: 'Laica NIT',
-//       description: 'Laboratório de robótica campus CNAT',
-//       created_by: 'cea7e1d2-f3ed-4f31-a912-304bf24b11f9',
-//     },
-//   });
+async function main() {
+  await prisma.environment.create({
+    data: {
+      name: 'Laica NIT',
+      description: 'Laboratório de robótica campus CNAT',
+      created_by: 'cea7e1d2-f3ed-4f31-a912-304bf24b11f9',
+    },
+  });
 
-//   await prisma.environment.create({
-//     data: {
-//       name: 'Laica SGA',
-//       description: 'Laboratório de robótica campus São Gonçalo dos Amarantes',
-//       created_by: 'cea7e1d2-f3ed-4f31-a912-304bf24b11f9',
-//     },
-//   });
+  await prisma.environment.create({
+    data: {
+      name: 'Laica SGA',
+      description: 'Laboratório de robótica campus São Gonçalo dos Amarantes',
+      created_by: 'cea7e1d2-f3ed-4f31-a912-304bf24b11f9',
+    },
+  });
 
-//   await prisma.environment.create({
-//     data: {
-//       name: 'Exemplo GoRN',
-//       description: 'Ambiente de exemplo para o GoRN',
-//       created_by: 'cea7e1d2-f3ed-4f31-a912-304bf24b11f9',
-//     },
-//   });
-// }
+  await prisma.environment.create({
+    data: {
+      name: 'Exemplo GoRN',
+      description: 'Ambiente de exemplo para o GoRN',
+      created_by: 'cea7e1d2-f3ed-4f31-a912-304bf24b11f9',
+    },
+  });
+}
 
 // main()
 //   .catch((error) => {

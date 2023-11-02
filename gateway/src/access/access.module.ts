ac1e673746e0090722adfe8d7c06b26e926721ed
HilquiasAbias <hilquias.dev@gmail.com>
2023-11-02 17:29:46 -0300

    corrigindo bug do gateway em erro no módulo de logs

--- gateway/src/access/access.module.ts
+++ gateway/src/access/access.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AccessService } from './access.service';
 import { AccessController } from './access.controller';
-import { HttpModule } from '@nestjs/axios';
+import { HttpModule, HttpService } from '@nestjs/axios';
 
 @Module({
   imports: [HttpModule],
   controllers: [AccessController],
-  providers: [AccessService]
+  providers: [AccessService, HttpService]
 })
 export class AccessModule {}

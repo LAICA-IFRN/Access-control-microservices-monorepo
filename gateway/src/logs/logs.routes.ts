d71ce424dd7b9f4d1756cd7c547d6b20198bd2b0
HilquiasAbias <hilquias.dev@gmail.com>
2023-11-02 17:44:39 -0300

    free palestine

--- gateway/src/logs/logs.routes.ts
+++ gateway/src/logs/logs.routes.ts
@@ -1,6 +1,6 @@
 export class LogsRoutes {
   private readonly auditLogsUrl = process.env.AUDIT_SERVICE_URL + '/logs';
-  private readonly accessLogsUrl = process.env.ACCESS_SERVICE_URL + '/access';
+  private readonly accessLogsUrl = process.env.AUDIT_SERVICE_URL + '/access';
 
   getAuditLogsUrl(): string {
     return this.auditLogsUrl;

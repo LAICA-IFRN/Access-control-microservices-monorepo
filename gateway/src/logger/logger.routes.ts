47ae58064c362682fba352d0e75b921faf76e0e3
HilquiasAbias <hilquias.dev@gmail.com>
2024-05-05 20:19:51 -0300

    versões dos serviços e pm2 config do logger

--- gateway/src/logger/logger.routes.ts
+++ gateway/src/logger/logger.routes.ts
@@ -1,3 +1,7 @@
 export class LoggerRoutes {
   private readonly loggerServiceUrl = process.env.LOGGER_SERVICE_URL;
+
+  createConfigLog(): string {
+    return `${this.loggerServiceUrl}/config`;
+  }
 }
